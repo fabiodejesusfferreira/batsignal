368e4090a10963bd8fdaace8f16aa6bb74f09e73
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:47:57 -0300

    Adicionado exportação com tipagem no parâmetro

--- src/components/Forms/string-form.tsx
+++ src/components/Forms/string-form.tsx
@@ -4,6 +4,7 @@ import { View, Text, TextInput } from "react-native";
 import { styles } from "./string-form-styles";
 
 export function Forms() {
+export function StringForms(props: StringFormsProps) {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>{props.label}</Text>
