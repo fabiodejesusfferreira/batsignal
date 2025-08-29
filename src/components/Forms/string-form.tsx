39275a7f8efbbe8009c1f22e84dbb67508b099ac
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:46:33 -0300

    Importando components para uso

--- src/components/Forms/string-form.tsx
+++ src/components/Forms/string-form.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, Text, TextInput } from "react-native";
 
 import { styles } from "./string-form-styles";
 
