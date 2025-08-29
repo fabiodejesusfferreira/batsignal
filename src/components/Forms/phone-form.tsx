97ad8d4978dde3366f86f968483c2ac21b50d800
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:51:55 -0300

    Adicionado TextInput ao componente PhoneForms
    Adicionado um campo TextInput no componente PhoneForms com suporte para placeholder personalizado, preenchimento automático, tratamento de entrada e validação. A entrada é limitada a 10 dígitos e utiliza um teclado numérico.

--- src/components/Forms/phone-form.tsx
+++ src/components/Forms/phone-form.tsx
@@ -10,6 +10,21 @@ export function PhoneForms() {
         
     <View style={formStyle.styles.container}>
       <Text style={formStyle.styles.label}>{props.label}</Text>
+      <TextInput
+        style={[formStyle.styles.input, displayInputBorderColor]}
+        placeholder={props.customplaceholder}
+        autoComplete={props.autocomplete}
+        onChangeText={(input) => {
+          handleValueChange(input);
+        }}
+        onFocus={handleFirstDigitOfInput}
+        value={inputValue}
+        onEndEditing={(input) => {
+          handleEndEditing(input.nativeEvent.text);
+        }}
+        keyboardType="number-pad"
+        maxLength={10}
+      ></TextInput>
     </View>
   );
 }
