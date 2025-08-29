9fc01bd5404efdd951f2aa6c4a87d4f1079d577a
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:12:50 -0300

    Components para os inputs de string

---
+++ src/components/Forms/string-form.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { styles } from './string-form-styles';
+
+export function Forms() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
