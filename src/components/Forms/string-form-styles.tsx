2267f7cf410926f39f7b8c2db990dc912d8654df
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:48:35 -0300

    Adicionado estilização para os dois components

--- src/components/Forms/string-form-styles.tsx
+++ src/components/Forms/string-form-styles.tsx
@@ -1,5 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {}
+  container: {
+    width: "90%",
+  },
+  label: {
+    fontWeight: "600",
+    fontSize: 16,
+    paddingBottom: 3
+  },
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 3,
+    boxShadow: "1px 1px 2px #00000065"
+  }
 });
