943cc4d5290e89f283a49b7612c8b001315205e0
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:13:00 -0300

    Components para os inputs de telefone

---
+++ src/components/Forms/phone-form.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./phone-form-styles";
+
+export function PhoneForms() {
+  return (
+    <View style={styles.container}>
+        
+    </View>
+  );
+}
