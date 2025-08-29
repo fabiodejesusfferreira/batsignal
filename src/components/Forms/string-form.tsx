140b21197118b2d609194a78f5d846887026cdd4
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:48:17 -0300

    Criação da interface dos parâmetros do component

--- src/components/Forms/string-form.tsx
+++ src/components/Forms/string-form.tsx
@@ -3,7 +3,69 @@ import { View, Text, TextInput } from "react-native";
 
 import { styles } from "./string-form-styles";
 
-export function Forms() {
+interface StringFormsProps {
+  label: string;
+  customplaceholder: string;
+  autocomplete:
+    | "additional-name"
+    | "address-line1"
+    | "address-line2"
+    | "birthdate-day"
+    | "birthdate-full"
+    | "birthdate-month"
+    | "birthdate-year"
+    | "cc-csc"
+    | "cc-exp"
+    | "cc-exp-day"
+    | "cc-exp-month"
+    | "cc-exp-year"
+    | "cc-number"
+    | "cc-name"
+    | "cc-given-name"
+    | "cc-middle-name"
+    | "cc-family-name"
+    | "cc-type"
+    | "country"
+    | "current-password"
+    | "email"
+    | "family-name"
+    | "gender"
+    | "given-name"
+    | "honorific-prefix"
+    | "honorific-suffix"
+    | "name"
+    | "name-family"
+    | "name-given"
+    | "name-middle"
+    | "name-middle-initial"
+    | "name-prefix"
+    | "name-suffix"
+    | "new-password"
+    | "nickname"
+    | "one-time-code"
+    | "organization"
+    | "organization-title"
+    | "password"
+    | "password-new"
+    | "postal-address"
+    | "postal-address-country"
+    | "postal-address-extended"
+    | "postal-address-extended-postal-code"
+    | "postal-address-locality"
+    | "postal-address-region"
+    | "postal-code"
+    | "street-address"
+    | "sms-otp"
+    | "tel"
+    | "tel-country-code"
+    | "tel-national"
+    | "tel-device"
+    | "url"
+    | "username"
+    | "username-new"
+    | "off";
+}
+
 export function StringForms(props: StringFormsProps) {
   return (
     <View style={styles.container}>
