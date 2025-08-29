984a6e4466f5a065407741000564ccb97c16dacd
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 18:26:03 -0300

    Adicionado importações das logos

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
+import batmanLogoLightmode from "../../../assets/batman-logo-lightmode.png";
+import batmanLogoDarkmode from "../../../assets/batman-logo-darkmode.png";
 
 import { styles } from './home-styles';
 
