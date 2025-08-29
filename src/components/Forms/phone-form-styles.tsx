c437564d40620d8025c77cda5176550c5ea028f6
fabiodejesusfferreira <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-08-29 19:05:51 -0300

    Estilização dos components de erro e input
    No caso, a estilização para a borda do input, de quando ele estiver correto ou incorreto

--- src/components/Forms/phone-form-styles.tsx
+++ src/components/Forms/phone-form-styles.tsx
@@ -2,4 +2,16 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {},
+  errorMessage: {
+    color: "#f00"
+  },
+  inputColorForCorrectValue: {
+    borderWidth: 1,
+    borderColor: '#000'
+  },
+  inputColorForInvalidValue: {
+    borderWidth: 1,
+    borderColor: '#f00',
+    backgroundColor: "rgba(255, 0, 0, 0.11)"
+  },
 });
